4eced246a20f3100076e2ee3fddaf4063e5a0472
wyj <fenxuan0930@gmail.com>
2017-06-14 10:25:23 +0800

    第一次提交

---
+++ app/src/main/java/com/example/collectionapplication/camera/SelectImageActivity.java
@@ -0,0 +1,19 @@
+package com.example.collectionapplication.camera;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.collectionapplication.R;
+
+/**
+ * Created by Administrator on 2017/6/14.
+ */
+
+public class SelectImageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_image);
+    }
+}
